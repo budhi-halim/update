ec81c03759eda57ac3320c80fb164c3b164d4ead
Budhi Halim <halimbudhiarto@gmail.com>
2024-11-11 11:30:08 +0700

    Sync

--- scripts/modules/utility.py
+++ scripts/modules/utility.py
@@ -1,6 +1,7 @@
 import json
 import pytz
 from datetime import datetime
+from functools import wraps
 
 def generate_json_file(dictionary, file_directory):
     json_file_content = json.dumps(dictionary)
@@ -17,8 +18,62 @@ def update_log_file(log_message, file_directory):
     with open(file_directory, 'a') as file:
         file.write(log_message)
 
-def log(start_log_message, finish_log_message):
-    def log_decorator(func):
+# def log(start_log_message, finish_log_message):
+#     def log_decorator(func):
+#         @wraps
+#         def wrapper(*args, **kwargs):
+#             # Prepare Constants
+#             file_directory = 'logs/log.txt'
+#             location = 'Asia/Jakarta'
+#             timestamp_format = '%Y-%m-%d %H:%M:%S'
+
+#             # Get Start Time
+#             start_time = get_local_time(location)
+#             start_time_formatted = start_time.strftime(timestamp_format)
+
+#             # Log Start
+#             log_message = f'{start_time_formatted} {start_log_message}\n'
+#             update_log_file(log_message, file_directory)
+
+#             try:
+#                 # Execute Function
+#                 func(*args, **kwargs)
+
+
+#                 # Get Finish Time
+#                 finish_time = get_local_time(location)
+#                 finish_time_formatted = finish_time.strftime(timestamp_format)
+
+#                 # Get Duration
+#                 duration = finish_time - start_time
+#                 hours, remainder = divmod(duration.total_seconds(), 3600)
+#                 minutes, seconds = divmod(remainder, 60)
+#                 duration = f'{int(hours)}h {int(minutes)}m {int(seconds)}s'
+
+#                 # Log Finish
+#                 log_message = f'{finish_time_formatted} {finish_log_message} ({duration})\n'
+#                 update_log_file(log_message, file_directory)
+
+#             except Exception as e:
+#                 # Get Error Time
+#                 error_time = get_local_time(location)
+#                 error_time_formatted = error_time.strftime(timestamp_format)
+
+#                 # Log Error
+#                 log_message = f'{error_time_formatted} An error occured: {e}\n'
+#                 update_log_file(log_message, file_directory)
+
+
+#         return wrapper
+#     return log_decorator
+
+class LogDecorator:
+    def __init__(self, start_log_message, finish_log_message):
+        self.start_log_message = start_log_message
+        self.finish_log_message = finish_log_message
+
+
+    def __call__(self, func):
         def wrapper(*args, **kwargs):
             # Prepare Constants
             file_directory = 'logs/log.txt'
@@ -61,6 +116,7 @@ def log(start_log_message, finish_log_message):
                 log_message = f'{error_time_formatted} An error occured: {e}\n'
                 update_log_file(log_message, file_directory)
 
-
         return wrapper
-    return log_decorator
+
+def log(start_log_message, finish_log_message):
+    return LogDecorator(start_log_message, finish_log_message)
--- scripts/wuwa.py
+++ scripts/wuwa.py
@@ -1,4 +1,10 @@
 with open('output/wuwa/wuwa.txt', 'w') as file:
     file.write('wuwa success')
 
-print('WUWA DONE')
+from modules.utility import *
+
+@log('WUWA START', 'WUWA FINISH')
+def main():
+    print('WUWA DONE')
+
+main()
